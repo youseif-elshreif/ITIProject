32c2f730e5996c79983ec6f4ff1fc1e581fb562a
Youseif Elshreif <youseif.elshreif@gmail.com>
2025-08-07 03:02:59 +0300

    first2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://jsonserver-production-74c8.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
